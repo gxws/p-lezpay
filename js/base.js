b94ff9ddaa1fa507cbc9c3a723971a34f807970d
xiewulong <xiewulong@vip.qq.com>
2014-11-14 14:05:57 +0800

    first commit

---
+++ js/base.js
@@ -0,0 +1,232 @@
+/*!
+ * base
+ * create: 2014/9/29
+ */
+
+/**
+ * 基础模块
+ * @module base
+ * @since p64
+ */
+(function(window, undefined){
+
+	/**
+	 * 基础模块 - 交互逻辑
+	 * @class base
+	 * @since p64
+	 */
+	var	$win	= $(window),
+		doc		= document,
+		$doc	= $(doc),
+		loc		= location,
+		WS		= window.WS || (window.WS = {}),
+		init	= {},							//启动时加载程序
+		base	= WS.base || (WS.base = {});	//接口程序
+
+/**************************** begin p64 ****************************/
+
+	/**
+	 * 初始化事件
+	 * @method base.events
+	 * @since p64
+	 * @return {none}
+	 */
+	init.events = function(){
+		$doc.on('input', '[data-verify]', function(){
+			$('[data-verify-prompt]').html('');
+		}).on('submit','.J_x_form_login',function(){
+			var $this = $(this);
+			return base.loginSubmit($this);
+		}).on('submit','.J_j_form',function(){//N种验证方式
+			var $form = $(this);
+			return base.nionpaySubmit($form)
+		}).on('submit','.J_j_idform',function(){//银行卡号验证
+			var $form = $(this);
+			return base.idSubmit($form)
+		}).on('focus','.J_j_fm input',function(){//获取焦点,错误提示消失
+			$(this).parent().next('.error').text('');
+		}).on('focus','.J_j_fm input[data-type=vcode]',function(){//获取焦点,错误提示消失
+			$(this).size() && $('.J_j_vcode').next('.error').text('');
+		}).on('click','.J_j_gain',function(){//点击获取验证码
+			var $this = $(this),
+          $phone = $('[data-type=phone]'),
+		    	vphone = $phone.val();
+		  if($phone.size()){
+		  	if(vphone==''){
+		  		$phone.parent().next().html('<i class="glyphicon glyphicon-remove-sign"></i>' + '请输入手机号');
+		  		return false;
+		  	}
+		  	base.gAin($this,vphone,function(){//回调倒计时
+					var time = null,
+					    s 	 = 60,
+					    text ='',
+					    vhref = $this.attr('href');
+					$this.removeAttr('href');
+					$this.addClass('disabled')
+					$('[data-type=vcode]').focus();
+					time = setInterval(function(){
+						s--;
+						text = s<=0 ? '获取验证码' : s + '秒',
+						$this.text(text);
+						if(s<=0){
+							clearInterval(time);
+							$this.attr('href',vhref);
+							$this.removeClass('disabled');
+						}
+					},1000);
+				});
+		  }
+			return false;
+		}).on('input','.J_k_phone',function(){//344方式显示手机号码
+			var		$this	=	$(this),
+					$val	=	$this.val(),
+					$noSpac	=	$val.replace(/\W/g, ""),
+					str		=	'';
+			if($win.width() < 768)return;
+			for(i=0;i<$noSpac.length;i++){
+				str += ((i-3) % 4?'':' ') + $noSpac[i];
+			}
+			$this.val(str);
+			//this.setSelectionRange(0,0);
+			//$.cookie('phone',str);//启用cookie
+			return false;
+		}).on('input','.J_k_cardnum',function(){//4443方式显示卡号
+			var		$this	=	$(this),
+					$val	=	$this.val(),
+					$noSpac	=	$val.replace(/\W/g, ""),
+					str		=	'';
+			$val	=	$noSpac;
+			for(i=0;i<$noSpac.length;i++){
+				str += ( i % 4?'':i == 0?'':' ') + $val[i];
+			}
+			$this.val(str);
+			return false;
+		}).on('submit','.J_k_form',function(){//注册验证
+			var $form = $(this);
+			return base.registerSubmit($form);
+		}).on('focus', '[data-formName]', function(){//获取焦点，错误消失
+			$('[data-error]').html('');
+		}).on('change', '.J_c_intprice,.J_c_surprice',function(){
+			if($(this).size()){
+				base.setTotalPrice();
+			}
+		}).on('submit','.J_x_submitToPay',function(){//选择银行进行支付
+			var $this	= $(this);
+			if($this.find('.weixin input[type=radio]').prop('checked') && window.WeixinJSBridge && base.wxCallback){
+				 return base.wxGoToPay(this);
+			}
+		})
+		.on('click','.c_paywaybox label',function(){//选择付款方式（银行、淘宝、微信）
+			var $this = $(this);
+			if($('.J_c_paybox').hasClass('on')){
+				return false;
+			}
+			else{
+				$('.J_c_p64slelab.on').css('border','1px solid #969696');
+				$this.addClass('on').css('border','1px solid #00a0e6');
+			}			
+		}).on('submit','.J_c_p64safety',function(){//银行卡号验证
+			var $form = $(this);
+			return base.p64safety($form)
+		}).on('submit','.J_c_p64safetypwd',function(){//银行卡号验证
+			var $form = $(this);
+			return base.p64safetypwd($form)
+		})
+		// .on('click','.J_k_click',function(){//p79  按键背景色变化
+		// 	$('.J_k_click').removeClass('click');
+		// 	$(this).addClass('click');
+		// }).on('click','.J_k_clickpage',function(){
+		// 	$('.J_k_clickpage').removeClass('clickpage');
+		// 	$(this).addClass('clickpage');
+		// })
+		.on('click','.J_K_sel',function(e){//下拉菜单点击事件
+			$('.J_K_sel').addClass('clicksel');
+			e.stopPropagation();
+		}).on('click',document,function(e){//全局点击事件
+			var	$sel	=	$('.J_K_sel');
+			if($sel.size()){
+				$sel.removeClass('clicksel');
+			}
+		}).on('change','.J_k_volCode',base.k_change);//协议判断
+		
+		aLert($('.J_j_validity'),'j_help','validity','click hover');//银行卡有效期帮助提示
+		aLert($('.J_j_afthree'),'j_help','afthree','click hover');//银行卡末三位帮助提示
+		function aLert($obj,boxClass,css,action){//弹出帮助提示框
+			$obj.tooltip({
+				container: 'body',
+				template: '<div class="tooltip '+ boxClass +'" role="tooltip"><div class="tooltip-inner ' + css + '"></div></div>',
+				trigger: action
+			});
+		}
+	};
+
+	/**
+	 * 判断注册协议是否阅读
+	 * @method base.k_change
+	 * @since p64
+	 * @return {none}
+	 */
+	init.k_change = function(){
+		$('.J_k_volCode').size() && $('.J_k_regBtn').prop('disabled', !$('.J_k_volCode').get(0).checked);
+	}
+
+	/**
+	 * 判断是否微信开启页面
+	 * @method base.wxApi
+	 * @since p64
+	 * @return {none}
+	 */
+	init.wxApi = function(){
+		if(!$('.J_x_submitToPay').size() || _getWxVersion() < 5)return;
+		var _apiCheck,
+			$form	= $('.J_x_submitToPay'),
+			time	= 10;
+		(_apiCheck = function(){
+			if(window.WeixinJSBridge){
+				$form.find('label.weixin').show();
+				return;
+			}
+			if(!time)return;
+			time--;
+			setTimeout(_apiCheck, 1000);
+		})();
+		function _getWxVersion(){
+			var wxTag	= 'MicroMessenger',
+				agent	= navigator.userAgent,
+				index	= agent.indexOf(wxTag);
+			return index < 0 ? 0 : agent.slice(index + 15, index + 16, 1) ;
+		}
+	};
+
+	/**
+	 * 读取cookie中的值
+	 * @method base.cookie
+	 * @since p64
+	 * @return {none}
+	 */
+	/*init.cookie = function(){
+		$('.J_k_phone').val($.cookie('phone'));
+		$.cookie('phone','',-1);
+	};*/
+
+	/**
+	 * 读取cookie中的值
+	 * @method base.cookie
+	 * @since p64
+	 * @return {none}
+	 */
+	base.c_discheck = function(disabled){
+		if(disabled == 1){
+			$('.J_c_paybox').removeClass('on');
+		}
+		else if(disabled == 0){
+			$('.J_c_paybox').addClass('on');
+		}
+		$('.J_c_paybox input[type=radio]').prop('disabled', !disabled);
+	}
+
+/**************************** end p64 ****************************/
+
+	$.extend(base, $.loader(init));
+
+})(window);
---
+++ js/baseAjax.js
@@ -0,0 +1,327 @@
+/*!
+ * baseAjax
+ * create: 2014/9/29
+ */
+
+/**
+ * 基础模块
+ * @module base
+ * @since p64
+ */
+(function(window, undefined){
+
+    /**
+	 * 基础模块 - 业务逻辑
+	 * @class baseAjax
+	 * @since p64
+	 */
+    var	$win	= $(window),
+		$doc	= $(document),
+		loc		= location,
+		WS		= window.WS || (window.WS = {}),
+		init	= {},							//启动时加载程序
+		base	= WS.base || (WS.base = {});	//接口程序
+
+/**************************** begin p64 ****************************/
+
+	/**
+	 * 银行卡信息验证
+	 * @method base.nionpaySubmit
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	var vText ='';
+	base.nionpaySubmit = function($form){
+		var $name   = $form.find('[data-type=carname]'),
+		    $idcard = $form.find('[data-type=idcard]'),
+		    vidcard = $idcard.val(),
+		    $passwd = $form.find('[data-type=passwd]'),
+		    vpasswd = $passwd.val(),
+		    $phone  = $form.find('[data-type=phone]'),
+		    vphone  = $phone.val(),
+		    $validity  = $form.find('[data-type=validity]'),
+		    vvalidity  = $validity.val(),
+		    $afterthree  = $form.find('[data-type=afterthree]'),
+		    vafterthree = $afterthree.val(),
+		    $vcode  = $form.find('[data-type=vcode]'),
+		    vvcode= $vcode.val();
+		if($name.size()){
+			vText = $name.val()=='' ? '姓名不能为空' : '';
+			if(vText!=''){
+			 	return error($name);
+			}
+		}
+		if($idcard.size()){
+			vText = !(vidcard.length==6) ? '请输入身份证的最后6位！' : '';
+			if(vText!=''){
+			 	return error($idcard);
+			}
+		}
+		if($passwd.size()){
+			vText = !(vpasswd.replace(/\D/g,'').length==6) ? '银卡密码只能为6位' : '';
+			if(vText!=''){
+			 	return error($passwd);
+			}
+		}
+		if($phone.size()){
+			vText = !(vphone.replace(/\D/g,'').length==11) ? '手机号只能是11位数字！' : '';
+			if(vText!=''){
+			 	return error($phone);
+			}
+		}
+		if($validity.size()){
+			vText = !(vvalidity.replace(/\D/g,'').length==4) ? '有效期只能是4位数字！' : '';
+			if(vText!=''){
+			 	return error($validity);
+			}
+		}
+		if($afterthree.size()){
+			vText = !(vafterthree.replace(/\D/g,'').length==3) ? '请输入卡背面最后3位数字！' : '';
+			if(vText!=''){
+			 	return error($afterthree);
+			}
+		}		
+		if($vcode.size()){
+			vText = !(vvcode.replace(/\D/g,'').length==6) ? '短信验证码只能是6位数字！' : '';
+			if(vText!=''){
+			 	$('.J_j_vcode').next('.error').html('<i class="glyphicon glyphicon-remove-sign"></i>' + vText);
+			 	return false;
+			}
+		}
+	}
+	/**
+	 * 银行卡号验证
+	 * @method base.idSubmit
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	base.idSubmit = function($form){
+		var $id   = $form.find('[data-type=cardnub]'),
+				vid   = $id.val().replace(/\D/g,'').length;
+		if($id.size()){
+			vText = !(vid == 16 || vid == 19) ? '付款银行卡只能是16或19位数字！' : '';
+			if(vText!=''){
+			 	return error($id);
+			}
+		}
+	}
+	function error($obj){
+		$obj.parent().next('.error').html('<i class="glyphicon glyphicon-remove-sign"></i>' + vText);
+		return false;
+	}
+
+	/**
+	 * 登录验证
+	 * @method base.loginSubmit
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	base.loginSubmit = function($form){
+		var error,
+			$phone	= $form.find('[data-verify=phone]'),
+			phone	= $phone.val().replace(/\s/g, ''),
+			$pwd	= $form.find('[data-verify=password]'),
+			pwd		= $pwd.val(),
+			$prompt	= $form.find('[data-verify-prompt=login]');
+		if(phone == ''){
+			error = '请输入您注册的手机号！';
+			$phone.focus();
+		}else if(!/^\d{11}$/.test(phone)){
+			error = '手机号只能是11位数字！';
+			$phone.focus();
+		}else if(pwd == ''){
+			error = '请输入密码！';
+			$pwd.focus();
+		}
+		if(error){
+			$prompt.html('<i class="glyphicon glyphicon-remove-sign"></i>' + error);
+			return false;
+		}
+	}
+	/**
+	 * 获取验证码
+	 * @method base.gAin
+	 * @since p64
+	 * @param {object} $btn 
+	 * @param {function} fn 
+	 * @return {bool}
+	 */
+	base.gAin = function($btn,vphone,fn){
+		base.ajaxgo($btn.attr('href'),function(d){
+			d.status==1 ? fn() : $('.J_j_error').html('<i class="glyphicon glyphicon-remove-sign"></i>' + d.message);
+		},{"status":"1","message":"您输入的手机号码不存在，请重新输入。"},{"phone":vphone.replace(/\D/g, '')});
+	};
+
+	/**
+	 * 兼容本地demo的ajax方法
+	 * @method base.ajaxgo
+	 * @since p61
+	 * @param {string} url 请求链接
+	 * @param {function} [fn] 方法
+	 * @param {number|string|object} [demo] 测试数据
+	 * @param {object} [data] 数据
+	 * @param {string} [method=post] 请求方式
+	 * @param {string} [datatype=json] 返回数据的格式
+	 * @return {none}
+	 */
+	base.ajaxgo = function(url, fn, demo, data, method, datatype){
+		fn = fn || Fn;
+		url == 'javascript:;' ? fn(demo) : $[method || 'post'](url, data, fn, datatype || 'json');
+	};
+
+
+	 /**
+	 * 找回密码，输入手机号码
+	 * @method base.p64safety
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	base.p64safety = function($form){
+		var error,
+			$phone	= $form.find('[data-verify=phone]'),
+			phone	= $phone.val().replace(/\s/g, ''),
+			$code	= $form.find('[data-verify=code]'),
+			code	= $code.val(),
+			errortxt= '<i class="glyphicon glyphicon-remove-sign"></i>';
+		if(phone == ''){
+			$phone.parent('.pa').next('.error').html(errortxt +'请您输入手机号！');
+			$phone.focus();
+			return false;
+		}else if(!/^\w{11}$/.test(phone)){
+			$phone.parent('.pa').next('.error').html(errortxt +'手机号应该是11位喔。');
+			$phone.focus();
+			return false;
+		}else if(!/^\d+$/.test(phone)){
+			$phone.parent('.pa').next('.error').html(errortxt +'手机号全都是数字喔。');
+			$phone.focus();
+			return false;
+		}else if(code == ''){
+			$code.parent('.pa').next('.error').html(errortxt +'请输入验证码！');
+			$code.focus();
+			return false;
+		}else if(!/^\d{6}$/.test(code)){
+			$code.parent('.pa').next('.error').html(errortxt +'验证码只能是6位数字！');
+			$code.focus();
+			return false;
+		}
+	}
+
+
+	 /**
+	 * 找回密码，设置新密码
+	 * @method base.p64safetypwd
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	base.p64safetypwd = function($form){
+		var error,
+			$pwd	= $form.find('[data-verify=pwd]'),
+			pwd		= $pwd.val(),
+			$agpwd	= $form.find('[data-verify=agpwd]'),
+			agpwd	= $agpwd.val(),
+			errortxt= '<i class="glyphicon glyphicon-remove-sign"></i>';
+		if(pwd == ''){
+			$pwd.parent('.pa').next('.error').html(errortxt +'请输入密码！');
+			$pwd.focus();
+			return false;
+		}else if(!/^\d{6}$/.test(pwd)){
+			$pwd.parent('.pa').next('.error').html(errortxt +'密码只能是6位数字喔！');
+			$pwd.focus();
+			return false;
+		}else if(agpwd == ''){
+			$agpwd.parent('.pa').next('.error').html(errortxt +'请输入确认密码！');
+			$agpwd.focus();
+			return false;
+		}else if(agpwd != pwd){
+			$agpwd.parent('.pa').next('.error').html(errortxt +'输入的确认密码和密码不一致喔！');
+			$agpwd.focus();
+			return false;
+		}
+	}
+
+
+	/**
+	 * 注册验证
+	 * @method base.registerSubmit
+	 * @since p64
+	 * @param {object} $form 表单对象
+	 * @return {bool}
+	 */
+	base.registerSubmit = function($form){
+		var vText		= '',
+			$phone		= $form.find('[data-formName=phone]'),
+			phone		= $phone.val(),
+			$pwd		= $form.find('[data-formName=password]'),
+			pwd			= $pwd.val(),
+			$conpwd		= $form.find('[data-formName=conPassword]'),
+			conpwd		= $conpwd.val(),
+			$verCode	= $form.find('[data-formName=verCode]'),
+			verCode		= $verCode.val();
+		if(!(/^\d{11}$/.test(phone.replace(/\W/g, "")))){
+			error($phone, '手机号只能是11位数字！');
+			return false;
+		}else if(!/^\d{6}$/.test(pwd)){
+			error($pwd,'密码只能是6位数字喔');
+			return false;
+		}else if(pwd != conpwd){
+			error($conpwd,'输入的确认密码和密码不匹配喔！');
+			return false;
+		}else if(!/^\d{6}$/.test(verCode)){
+			error($verCode, '短信验证码只能是6位数字！');
+			return false;
+		}
+
+		//$.cookie('phone','',-1);//启用cookie
+
+		function error($obj, str){
+			$obj.parents('.form-group').find('.error').html('<i class="glyphicon glyphicon-remove-sign"></i>' + str);
+		}
+	};
+
+	/**
+	 * 微信提交后回调
+	 * @property base.wxCallback
+	 * @since p64
+	 * @type object
+	 * @default null
+	 */
+	base.wxCallback = null;
+
+	/**
+	 * 获取微信网页支付接口数据信息
+	 * @method base.wxGoToPay
+	 * @since p64
+	 * @param {object} form 表单对象
+	 * @return {bool}
+	 */
+	base.wxGoToPay = function(form){
+		$.xajax($(form).attr('data-ajax-getWxApiInfoUrl'), function(d){
+			+ d.status && WeixinJSBridge.invoke('getBrandWCPayRequest',d.data, base.wxCallback);
+		}, {
+			'status': '1',
+			'data': {
+				'appId': 'wx2e8e96fbaad951cf',
+				'timeStamp': '1413446649',
+				'nonceStr': 'YZoasrffkPWHMlaI',
+				'package': 'bank_type=WX&body=%E6%94%AF%E4%BB%98%E6%B5%8B%E8%AF%95%E8%BF%9E%E6%8E%A5&fee_type=1&input_charset=GBK&notify_url=https%3A%2F%2Fweixin.code.lezpay.cn%2Fwxpay%2FreceiptNotify&out_trade_no=0%3A0%3A0%3A0%3A0%3A0%3A0%3A1&partner=1219711201&spbill_create_ip=0000225&total_fee=140&sign=4D2DCFF73195187666814D8A176EDEF9',
+				'signType': 'SHA1',
+				'paySign': '55ddfac3af969cf81f2042d1d127bbf590479a1b'
+			}
+		}, {
+			jifen: $('.J_c_intprice').prop('checked') ? 1 : 0,
+			yue: $('.J_c_surprice').prop('checked') ? 1 : 0,
+			channelId: $('.J_j_wexin').val()
+		});
+		return false;
+	};
+
+/**************************** end p64 ****************************/
+
+	$.extend(base, $.loader(init));
+
+})(window);
